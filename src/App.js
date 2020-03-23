78d814446d8d76ae605e204a20fec346c37c5893
yotsugi <harukipdr@gmail.com>
2020-03-23 14:26:22 +0900

    大体

--- src/App.js
+++ src/App.js
@@ -1,18 +1,150 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { teamAlpha, teamBlabo, getImage, result, stream } from './data';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="contents">
+        <p className="title">闇のドラフト大会</p>
+        <p className="p_team">チーム</p>
+        <div className="team">
+          <ul>
+            {teamAlpha.map((player, i) => (
+              <li key={i}>
+                <ul>
+                  {player.map((player, i) => (
+                    <li className={i === 0 ? "player" : "weapon"}>{player}</li>
+                  ))}
+                </ul>
+              </li>))}
+          </ul>
+          <ul>
+            {teamBlabo.map((player, i) => (
+              <li key={i}>
+                <ul>
+                  {player.map((player, i) => (
+                    <li className={i === 0 ? "player" : "weapon"}>{player}</li>
+                  ))}
+                </ul>
+              </li>))}
+          </ul>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="allResult">
+          {result.map((match, i) => (
+            <div className="result" key={i}
+              style={{
+                backgroundImage: `url(${getImage(match.stage)})`,
+                backgroundRepeat: "no-repeat",
+                backgroundPosition: "center",
+                backgroundColor: "rgba(240,255,255,0.5)",
+                backgroundBlendMode: "lighten",
+                backgroundSize: "cover"
+              }}
+            >
+              <div style={{
+                display: "flex",
+                justifyContent: "space-evenly"
+              }}>
+                <div style={{ display: "flex" }}>
+                  <p className="p_matchCount">第{i + 1}試合</p>
+                  <img src={getImage(match.rule)} alt="rule" style={{ width: "4rem" }} />
+                </div>
+              </div>
+              <div className="gameRatio">
+                <p>アルファ:{match.ratioA}</p>
+                <p>ブラボー:{match.ratioB}</p>
+              </div>
+              <div className="gameMeter">
+                <div style={{ display: "flex", justifyContent: "strech" }}>
+                  <div className="alpha"
+                    style={{
+                      width: `${match.ratioA / (match.ratioA + match.ratioB) * 10}rem`,
+                      height: "25px",
+                      backgroundColor: `rgb(53, 255, 70)`
+                    }} />
+                  <div className="blabo"
+                    style={{
+                      width: `${match.ratioB / (match.ratioA + match.ratioB) * 10}rem`,
+                      height: "25px",
+                      backgroundColor: `rgb(255, 75, 129)`
+                    }}
+                  />
+                </div>
+              </div>
+              <div style={{
+                display: "flex",
+                justifyContent: "space-evenly"
+              }}>
+                <div style={{
+                  display: "flex",
+                  fontWeight: "700"
+                }}>
+                  <p style={{
+                    color: "orange",
+                    WebkitTextStroke: "0.5px #FFF"
+                  }}>WIN：</p>
+                  <p>{match.WIN}チーム</p>
+                </div>
+              </div>
+              <a href={match.link}>試合リンク</a>
+            </div>
+          ))}
+          <div className="overAllResult">
+            <div style={{
+              display: "flex",
+              justifyContent: "space-evenly"
+            }}>
+              <ul>
+                <li>アルファ</li>
+                <li>5勝</li>
+              </ul>
+              <ul>
+                <li>ブラボー</li>
+                <li>0勝</li>
+              </ul>
+            </div>
+            <p>優勝 アルファチーム</p>
+            <p className="winner">Congratulations!</p>
+            <img src={getImage("アルファ")} alt="win team"
+              style={{
+                width: "96%"
+              }} />
+          </div>
+
+          <div>
+            <p>more streams</p>
+            <ul>
+              {stream.map((stream, i) => (
+                <li key={i}>
+                    <div style={{
+                      display: "flex",
+                      justifyContent:"start",
+                      position:"relative",
+                      left:"10rem",
+                    }}>
+
+                      <a href={stream.link}>
+                        <img src={getImage(stream.stream)} alt="stramsite"
+                          style={{
+                            borderRadius: "5px",
+                            width: "80px"
+                          }} />
+                      </a>
+                      <p style={{
+                        position:"absolute",
+                        fontSize: "1.5rem",
+                        left:"85px",
+                        bottom:"30px"
+                      }}>{stream.name}</p>
+                    </div>
+                </li>
+              ))}
+            </ul>
+
+            <p>制作 よつぎ</p>
+          </div>
+        </div>
       </div>
     );
   }
--- src/data.js
+++ src/data.js
@@ -18,7 +18,7 @@ export const result = [
         "stage": "ショッツル鉱山",
         "ratioA": 76,
         "ratioB": 27,
-        "WIN": "みんと",
+        "WIN": "アルファ",
         "link": "https://youtu.be/Ig59TvNasMo?t=1920"
     },
     {
@@ -26,7 +26,7 @@ export const result = [
         "stage": "モズク農園",
         "ratioA": 100,
         "ratioB": 0,
-        "WIN": "みんと",
+        "WIN": "アルファ",
         "link": "https://youtu.be/Ig59TvNasMo?t=2465"
     },
     {
@@ -34,7 +34,7 @@ export const result = [
         "stage": "Bバスパーク",
         "ratioA": 84,
         "ratioB": 83,
-        "WIN": "みんと",
+        "WIN": "アルファ",
         "link": "https://youtu.be/Ig59TvNasMo?t=2760"
     },
     {
@@ -42,7 +42,7 @@ export const result = [
         "stage": "アンチョビットゲームズ",
         "ratioA": 76,
         "ratioB": 0,
-        "WIN": "みんと",
+        "WIN": "アルファ",
         "link": "https://youtu.be/Ig59TvNasMo?t=3153"
     },
     {
@@ -50,10 +50,43 @@ export const result = [
         "stage": "タチウオパーキング",
         "ratioA": 85,
         "ratioB": 35,
-        "WIN": "みんと",
+        "WIN": "アルファ",
         "link": "https://youtu.be/Ig59TvNasMo?t=3571"
     }
-]
+];
+
+export const stream = [
+    {
+        "name": "よつぎ",
+        "stream": "youtube",
+        "link": "https://www.youtube.com/channel/UCZ5DicEzH5eWivxdaAiXuFQ"
+    },
+    {
+        "name": "むめい",
+        "stream": "twitch",
+        "link": "https://www.twitch.tv/mumei_bk"
+    },
+    {
+        "name": "そららら",
+        "stream": "mixer",
+        "link": "https://mixer.com/SRRR"
+    },
+    {
+        "name": "やまいも",
+        "stream": "twitch",
+        "link": "https://www.twitch.tv/torotorotororo"
+    },
+    {
+        "name": "ケケ(serizawagod)",
+        "stream": "twitch",
+        "link": "https://www.twitch.tv/serizawagod"
+    },
+    {
+        "name": "りんご",
+        "stream": "twitch",
+        "link": "https://www.twitch.tv/ringorz"
+    }
+];
 
 import img_area_logo from "./picture/area.png";
 import img_hoko_logo from "./picture/hoko.png";
@@ -64,9 +97,13 @@ import stg_anchobi from "./picture/アンチョビットゲームズ.png";
 import stg_shotturu from "./picture/ショッツル鉱山.png";
 import stg_mozuku from "./picture/モズク農園.png";
 import stg_tatiuo from "./picture/タチウオパーキング.png";
+import team_win from "./picture/winTeam.png"
+import logo_youtube from "./picture/youtube_logo.png"
+import logo_twitch from "./picture/twitch_logo.png";
+import logo_mixer from "./picture/mixer_logo.png"
 
 export function getImage(stage) {
-    let ret;
+    let ret = img_hoko_logo;
     switch (stage) {
         case "ガチホコバトル":
             ret = img_hoko_logo;
@@ -95,8 +132,21 @@ export function getImage(stage) {
         case "タチウオパーキング":
             ret = stg_tatiuo;
             break;
+        case "アルファ":
+            ret = team_win;
+            break
+        case "youtube":
+            ret = logo_youtube;
+            break;
+        case "twitch":
+            ret = logo_twitch;
+            break;
+        case "mixer":
+            ret = logo_mixer;
+            break;
         default:
             ret = img_hoko_logo;
             break;
     }
+    return ret;
 }
